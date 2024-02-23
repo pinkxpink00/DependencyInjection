33753780ad5545e570abcda496d6985b5a94d6e0
pinkxpink <kyperu07@gmail.com>
2024-02-23 23:59:01 +0200

    Добавьте файлы проекта.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        var logger = new Logger();
+        logger.Log("Hello");
+    }
+}
+
+class SimpleLogService
+{
+    public void Write(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
+
+class Logger
+{
+    SimpleLogService simpleLogService = new SimpleLogService();
+    public void Log(string message) => simpleLogService.Write($"{DateTime.Now}-{message}");
+}
